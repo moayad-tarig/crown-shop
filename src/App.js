b029eec8b0002cabe1121d5a93e2587ce5d13d6e
moayad-tarig <software-moayad@gmail.com>
2021-10-15 02:59:51 -0700

    make home page with react app and sass

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HomePage from "./homePage.component";
+
+function App() {
+  return (
+    <div>
+      <div className="container">
+      <HomePage/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
